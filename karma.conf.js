33039898d32874c497d7aeaeb9ed9e7b5af058ba
Oliver Kovacs <oliver.kovacs@lensa.com>
2013-11-18 16:05:40 +0100

    Update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,54 +1,22 @@
-// Karma configuration
+/*global module */
+module.exports = function ( config ) {
+  'use strict';
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+  var files = [
+    'components/angular/angular.js',
+    'components/angular-mocks/angular-mocks.js',
+    'src/storageprovider.js',
+    'test/storageprovider.spec.js'
+  ];
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'components/angular/angular.js',
-  'components/angular-mocks/angular-mocks.js',
-  'src/storageprovider.js',
-  'test/storageprovider.spec.js'
-];
-
-// list of files to exclude
-exclude = [];
-
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
-
-// web server port
-port = 8080;
-
-// cli runner port
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+  config.set({
+    files : files,
+    basePath: '',
+    frameworks: ['jasmine'],
+    reporters: ['progress'],
+    browsers: ['Chrome', 'Firefox'],
+    autoWatch: false,
+    singleRun: true,
+    colors: true
+  });
+};
